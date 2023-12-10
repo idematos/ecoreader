f2cbbffd96cb519538c11c94151ee4e7d0c449dd
Guilherme <12400406+zanardob@users.noreply.github.com>
2023-12-10 15:26:56 +0100

    Rebase main

--- src/background.ts
+++ src/background.ts
@@ -7,12 +7,20 @@ browser.webRequest.onCompleted.addListener(
     const responseSize = contentLengthHeader?.value
       ? parseInt(contentLengthHeader.value, 10)
       : 0
+
+    console.log({
+      tabId: details.tabId,
+      url: details.url,
+      responseSize,
+    })
+
     browser.storage.local.get({ totalSize: 0 }).then((result) => {
       const newTotalSize = parseInt(result.totalSize, 10) + responseSize
-
       browser.storage.local.set({ totalSize: newTotalSize })
     })
   },
   { urls: ['<all_urls>'] },
   ['responseHeaders']
 )
+
+console.log('Background script loaded!')
