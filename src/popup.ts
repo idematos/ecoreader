08560e23c22a7847b153c4d01e147c85d1c4aae8
matosisabela <isabelavdmatos@gmail.com>
2023-12-10 10:21:44 +0100

    Show current domain

--- src/popup.ts
+++ src/popup.ts
@@ -14,3 +14,27 @@ document.addEventListener('DOMContentLoaded', () => {
     })
   }
 })
+
+const extractDomain = (url: string): string | null => {
+  const match = url.match(/^(?:https?:\/\/)?(?:www\.)?([^/?]+)/)
+  return match ? match[1] : null
+}
+
+const updateTabInfo = (): void => {
+  browser.tabs.query({ active: true, currentWindow: true }).then((tabs) => {
+    const activeTab = tabs[0]
+    if (activeTab.url) {
+      const domain = extractDomain(activeTab.url)
+      const urlElement = document.getElementById('currentDomain')
+      if (urlElement) {
+        urlElement.textContent = domain || ''
+      }
+    }
+  })
+}
+
+updateTabInfo()
+
+browser.tabs.onActivated.addListener(() => {
+  updateTabInfo()
+})
