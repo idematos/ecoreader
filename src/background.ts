15a819ffe23020cf0dff14d2cb6987d125522903
matosisabela <isabelavdmatos@gmail.com>
2023-12-11 12:09:25 +0100

    Remove unnecessary code

--- src/background.ts
+++
@@ -1,8 +0,0 @@
-browser.runtime.onMessage.addListener((request) => {
-  if (request.action === 'updateWhitelistLink') {
-    const linkText = request.isWhitelisted
-      ? 'REMOVE FROM WHITELIST'
-      : '+ ADD TO WHITELIST'
-    browser.browserAction.setBadgeText({ text: linkText })
-  }
-})
